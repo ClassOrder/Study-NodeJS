0ff95c19e747460a8bfdc9ca5f58abc196728af3
Sherlock E. Gift <32980429+ClassOrder@users.noreply.github.com>
2018-03-04 14:05:18 +0900

    Configure

---
+++ StdNode/testapp/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const config = require("./server/configure");
+const path = require("path");
+let app = express();
+
+app.set("port", process.env.PORT || 10808);
+app.set("views", path.join(__dirname, "views"));
+app = config(app); // 조금 직관적이지 않은 느낌 ..
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.listen(app.get("port"), () => {
+    console.log("Server up: http://localhost:"+app.get("port"));
+});
